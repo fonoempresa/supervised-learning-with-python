559cec84433c59f7c1d83d691515b1b71df6f27d
vsaitejaswie <saitejaswie@gmail.com>
2018-01-26 23:25:50 +0530

    Added Comments in digit_knn_plot

--- digits_knn_plot.py
+++ digits_knn_plot.py
@@ -1,14 +1,23 @@
+#import numpy
 import numpy as np
+#import datasets from sklearn
 from sklearn import datasets
+#import KNeighborsClassifier from sklearn.neighbors
 from sklearn.neighbors import KNeighborsClassifier
+#import train_test_split
 from sklearn.model_selection import train_test_split
+#import matplotlib
 import matplotlib.pyplot as plt
 
+#load digits dataset
 digits = datasets.load_digits()
 
+#load data from digits dataset
 X = digits.data
+#load the target data
 y = digits.target
 
+#Split arrays into random train and test subsets
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y)
 
@@ -17,6 +26,7 @@ print(neighbors)
 train_accuracy = np.empty(len(neighbors))
 test_accuracy = np.empty(len(neighbors))
 
+#loop
 for i, k in enumerate(neighbors):
     knn = KNeighborsClassifier(n_neighbors=k)
 
